2aaa83648b7ad1ccb896e8efe346ac1d80ac293b
davidmustea <davidmustea18@gmail.com>
2021-04-22 14:42:42 +0300

    help si var 1 cautare linkuri

--- main.py
+++ main.py
@@ -4,21 +4,36 @@ from bs4 import BeautifulSoup
 
 linkraw = input("Site-ul: ")
 
+#Vedem daca site-ul incepe cu 'www'
 while not linkraw.startswith('www'):
     print("Link-ul trebuie sa inceapa cu 'www'")
     linkraw = input("Site-ul: ")
 
-
+#Adaugare 'https://' la inceputul linkului
 link = f'https://{linkraw}'
 httprequest = requests.get(link)
 
+#Verificam daca site-ul e valid
 if httprequest.status_code == 200:
     print("Valid")
 if httprequest.status_code != 200:
     print("Invalid")
 
+#Obiectul soup
 src = httprequest.content
 soup = BeautifulSoup(src,'lxml')
 
-print(soup.find_all('a'))
+ceVreaUser = input("Ce facem cu linkul?\nScrie 'help' pentru optiuni.\n")
+
+#"help" pentru user
+if ceVreaUser.lower() == "help":
+    print("[1] Cautare linkuri")
+    print("[2] Cautare poze")
+
+#Varianta 1: Cautare linkuri
+if ceVreaUser == '1':
+    print("Cautam linkuri...")
+    print("Rezultate:")
+    print(soup.find_all('a'))
+
 
